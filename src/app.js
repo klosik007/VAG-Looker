7e64058b804fe39c7aeb25c36b8ed028933afe0b
Przemyslaw Klos <pklos1992@gmail.com>
2019-09-06 00:25:06 +0200

    added regex and checking for matches - sketch

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
-const fs = require('fs');
+const LoadFile = require('./components/LoadFile');
 const path = require('path');
 //const src = require('settings');
 
-function LoadFile(source){
-    return fs.readFileSync(source).toString();
-}
 
-var publicPath = path.join(__dirname, '../labels/4H-01.lbl');
-var file = LoadFile(publicPath);
+
+var publicPath = path.join(__dirname, '../labels/44-01.lbl');
+var file = LoadFile.LoadFile(publicPath);
 
 console.log(file);
 
-var regExp1 = /REDIRECT,(.{3}-.{3}-.{3})(.{0,4})\.(lbl|clb|LBL|CLB)/;//first part of redirect - name of label file in redirect file xx-yy
-var regExp2 = /,(.{3}-.{3}-.{3})/; //second part of REDIRECT - VAG number in redirect file xx-yy
-var regExp3 = /({3}-{3}-{3})/; //only VAG number in file's name
+var regExp1 = /(.{3}-.{3}-.{3})(.{0,5})\.(lbl|clb|LBL|CLB)/g;//first part of redirect - name of label file in redirect file xx-yy
+var match = regExp1.exec(file);
+
+var regExp2 = /(.{3}-.{3}-.{3})/g; //second part of REDIRECT - VAG number in redirect file xx-yy
+                                    //or another approach - regex will be given as VAG number in browser and matched or not in file
+
+console.log(match[0]);
+// console.log(match2);
 //var regExp4 = 
+
+var directFileNameToLoad = path.join(__dirname, `../labels/${match[0]}`);
+var fileToLoad = LoadFile.LoadFile(directFileNameToLoad);
+
+console.log(fileToLoad);
