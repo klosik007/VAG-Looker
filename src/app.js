cf1468cc369200cee79360194ca2cbfd6caa0773
Przemyslaw Klos <pklos1992@gmail.com>
2019-09-10 07:22:40 +0200

    added regexes to perform labels search and onclick event to button - button doing all necessary stuff

--- src/app.js
+++ src/app.js
@@ -1,25 +1,58 @@
-const LoadFile = require('./components/LoadFile');
-const path = require('path');
-//const src = require('settings');
+import { LoadFile } from './components/LoadFile';
+import { settings } from './settings'; 
+
+
+//console.log(VAGNum);
+
+var button = document.getElementById("button").onclick = function(){
+    var VAGNum = document.getElementById("VAGNum").value;
+    if (!VAGNum) console.log("Insert text!");
+
+    var firstTwoLettersVAG = VAGNum.substring(0,1);
+
+    var driverList = document.getElementById("driverType");
+    var selectedDriver = driverList.options[driverList.selectedIndex].value;
+
+    var publicGeneralPath = `${settings.source}/Labels/${firstTwoLettersVAG}-${selectedDriver}`;
+    var file = LoadFile(publicGeneralPath);
+    if (!file){
+        throw "No general file!";
+    }
+
+    console.log(selectedDriver);
+    console.log(VAGNum);
+
+    console.log(file);
+    //console.log(VAGNum, driverType);
+
+    //var VAGNum = "441-907-557-D";
+    var matches2 = file.match(new RegExp(VAGNum, "is"));
+    if (!matches2){
+        throw "No such VAG num in label file!";
+    }
+
+    console.log(matches2[0]);
+
+
+    var regExp1 = /(.{3}-.{3}-.{3})(.{0,5})\.(lbl|clb|LBL|CLB)/gi;//first part of redirect - name of label file in redirect file xx-yy
+    var matches1 = file.match(regExp1);
+    console.log(matches1[0]);
+
+    if (matches1 && matches2){
+        var label = LoadFile(`${settings.source}/Labels/${matches1}`);
+        if (!label){
+            throw "File not found!";
+        }
+        //parsing the label file - TBC
+    }
+};
 
 
 
-var publicPath = path.join(__dirname, '../labels/44-01.lbl');
-var file = LoadFile.LoadFile(publicPath);
 
-console.log(file);
+// var directFileNameToLoad = settings.source + `/${match[0]}`;
+// var fileToLoad = LoadFile(directFileNameToLoad);
 
-var regExp1 = /(.{3}-.{3}-.{3})(.{0,5})\.(lbl|clb|LBL|CLB)/g;//first part of redirect - name of label file in redirect file xx-yy
-var match = regExp1.exec(file);
+//console.log(fileToLoad);
 
-var regExp2 = /(.{3}-.{3}-.{3})/g; //second part of REDIRECT - VAG number in redirect file xx-yy
-                                    //or another approach - regex will be given as VAG number in browser and matched or not in file
 
-console.log(match[0]);
-// console.log(match2);
-//var regExp4 = 
-
-var directFileNameToLoad = path.join(__dirname, `../labels/${match[0]}`);
-var fileToLoad = LoadFile.LoadFile(directFileNameToLoad);
-
-console.log(fileToLoad);
