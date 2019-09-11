ff0e767a144a65310fc4d1acedc68d9ff520fc8b
Przemyslaw Klos <pklos1992@gmail.com>
2019-09-11 23:52:37 +0200

    added try catch statement to prevent loading wrong data

--- src/components/LoadFile.js
+++ src/components/LoadFile.js
@@ -1,11 +1,16 @@
 export const LoadFile = (source) =>{
-    var labelFile = new XMLHttpRequest();
+
+    try {
+        var labelFile = new XMLHttpRequest();
         labelFile.open("GET", source, false); //request is synchronous, otherwise I couldn't saved answer content to variable so simple
         labelFile.send(null);
-    
-   
-        if (labelFile.readyState== 4 && labelFile.status == 200) {
+
+        if (labelFile.readyState == 4 && labelFile.status == 200) {
             var content = labelFile.responseText;
             return content;
-        } else return false;
+        }
+    } catch (error) {
+        return false;
+    }
+    
 }
