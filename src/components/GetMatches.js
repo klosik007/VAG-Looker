658f88d6797edd155329e0543ef1212cf1c98bb6
Przemyslaw Klos <pklos1992@gmail.com>
2019-09-29 14:20:20 +0200

    getting and returning whole matches from string - new function, no nedd to change it so far

---
+++ src/components/GetMatches.js
@@ -0,0 +1,9 @@
+export const GetMatches = (string, regex, index) => {
+    index || (index = 1); // default to the first capturing group
+    var matches = [];
+    var match;
+    while (match = regex.exec(string)) {
+      matches.push(match[index]);
+    }
+    return matches;
+  };
