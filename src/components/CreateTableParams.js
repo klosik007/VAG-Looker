c219d1dce290e35c65e75db075f1d3b53ea6c6fc
Przemyslaw Klos <pklos1992@gmail.com>
2019-11-17 16:24:59 +0100

    deleted comments, added possibility to hide each row of table

--- src/components/CreateTableParams.js
+++ src/components/CreateTableParams.js
@@ -1,15 +1,10 @@
 import { CreateTable } from './CreateTable';
+import { create } from 'domain';
 
 export class CreateTableParams extends CreateTable{
     constructor(object, type, label){
         super(object, type, label);
     }
-    
-    // boldParamNameGroup(text, regex){
-    //     if (match.length > 0){
-
-    //     }
-    // }
 
     createTableParams(array){
         var div = document.createElement("div");
@@ -23,47 +18,64 @@ export class CreateTableParams extends CreateTable{
         table.style.border = "2px solid #456789";
         table.createCaption().innerHTML = `<b>Results: ${this.type}</b>`;
 
-        //ar printTable = (labelsHead, array) => {
-            var row = table.insertRow();
-
-            // for (var i = 0; i < labelsHead.length; i++) {
-            //     var headerCell = document.createElement("th");
-            //     headerCell.innerHTML = labelsHead[i];
-            //     row.appendChild(headerCell);
-            // }
-
             var tableLen = array[0].length;
             var tableWidth = array.length;
-            console.log(tableLen, tableWidth);
+          
+            //console.log(tableLen, tableWidth);
             for(var i = 0; i < tableLen; i++){
                 var tr = table.insertRow();
-                // var td2 = tr.insertCell();
-                //     td2.appendChild(document.createTextNode(object[i][j]));
-                //     td2.style.border = "1px solid #456";    
-                // th1.appendChild(document.createTextNode('Adaptation value'));
-                // th1.style.border = "1px solid #456";
+                tr.setAttribute("id", `param${i}`);
+                //tr.style.width = "700px";
                 for (var j = 0; j < tableWidth; j++){
                     var td = tr.insertCell();
                     td.appendChild(document.createTextNode(array[j][i]));
-                    td.style.border = "1px solid #456";    
+                    td.style.border = "1px solid #456";
+                    // td.style.display = "inline";
+                    td.style.width = "225px";
+                    td.style.textAlign = "center";
                 }
             }
 
             putTable.insertAdjacentElement("beforeend", table);
 
+        //create buttons and set attributes
+        for (var i=0; i < tableLen ; i++){
+            var button = document.createElement("button");
+            button.setAttribute("id", `hideParam${i}`);
+            button.innerText = "+/-";
+            button.style.position = "absolute";
+            button.style.left = `790px`;
+            var heightFromTop = document.getElementById(`param${i}`).getBoundingClientRect().height;
+            var offsetTop = document.getElementById(`param${i}`).offsetTop;
+            button.style.top = `${offsetTop + heightFromTop/2 +335}px`;
+            div.appendChild(button);
+        }
+
+        
+        var clicked = false;
+        var buttons = document.getElementsByTagName("button");
+
+        //set onclick event
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].onclick = function(e){
+                if (!clicked){
+                    clicked = true;
+                    document.getElementById(`param${i}`).style.display = 'none';
+                }else {
+                    clicked = false;
+                    document.getElementById(`param${i}`).style.display = '';
+                }
+            };
+        }
+        
+        //set color on group names
             var cells = document.getElementById("resultTableParams").getElementsByTagName("td");
-            var rows = document.getElementById("resultTableParams").getElementsByTagName("tr");
-            console.log(cells);
-            console.log(rows);
-            var row = 1;
             for (var i = 0; i < cells.length; i++) {
                 if (cells[i].innerHTML == "0" || cells[i].innerHTML == "000" ) {
-                    cells[i].style.backgroundColor = "red";
-                    cells[i-1].style.backgroundColor = "red";
-                    cells[i+1].style.backgroundColor = "red";
+                    cells[i].style.backgroundColor = "#FF55";
+                    cells[i-1].style.backgroundColor = "#FF55";
+                    cells[i+1].style.backgroundColor = "#FF55";
                 }
             }
-
         }
     }
-//}
